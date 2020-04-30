1de5517f1d31d5acf38d6586606b01693f932a79
dsvanidze <svanskivar@gmail.com>
2020-04-30 19:14:01 +0200

    Hello World setup

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import rasterio
+import pandas
+
+# Create a list of evenly-spaced numbers over the range
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
+
+print("Hello World")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Spatio-Temporal-Deep-Learning-Model-For-Coronavirus-Cases
+# Spatio-Temporal Deep-Learning Model For Coronavirus Cases
+
+This Deep Learning model will be able to predict number of coronavirus cases in China through Spatio-Temporal Data. 
