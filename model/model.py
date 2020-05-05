bcb5c377e309ed48d154dd5c5affd2091777195d
dsvanidze <svanskivar@gmail.com>
2020-05-05 23:39:43 +0200

    extend model with raster value. No improvement of the model performance

--- model/model.py
+++ model/model.py
@@ -1,20 +1,25 @@
+import matplotlib.pyplot as plt
 from keras.callbacks import EarlyStopping
-from keras.layers import Dense
+from keras.layers import Dense, BatchNormalization, Dropout
 from keras.models import Sequential
 from keras.optimizers import Adam
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from reproduce import reproduce
+import numpy as np
 
 # This set seeds to make the result reproducible
 reproduce(0)
 
 # read in data using pandas
-data = pd.read_csv("data/csvs/cleandata.csv")
+data = pd.read_csv("data/csvs/cleandata_model.csv")
 
-data = data.groupby(["longitude", "latitude"],
-                    as_index=False).agg({"cases": "mean"})
+# data = data.groupby(["longitude", "latitude"], as_index=False).agg(
+#     "mean").drop(columns=["Org.ID"])
 
+
+# data.to_csv("data/csvs/cleandata_model.csv", index=False)
+print(data.head())
 
 # create a dataframe with all training data except the target column
 X = data.drop(columns=["cases"])
@@ -39,22 +44,56 @@ model = Sequential()
 n_cols = X_train.shape[1]
 
 # add model layers
-model.add(Dense(5, activation="relu", input_shape=(n_cols,)))
+model.add(Dense(20, activation="relu", input_shape=(n_cols,)))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+model.add(Dense(12, activation="relu"))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+model.add(Dense(12, activation="relu"))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+model.add(Dense(8, activation="relu"))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+model.add(Dense(8, activation="relu"))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
 model.add(Dense(4, activation="relu"))
-model.add(Dense(1))
+model.add(BatchNormalization())
+model.add(Dense(1, activation="relu"))
 
-optimizer = Adam(learning_rate=0.01)
+optimizer = Adam(learning_rate=0.000001)
 
 # compile model using mse as a measure of model performance
-model.compile(optimizer=optimizer, loss="mean_squared_error")
+model.compile(optimizer=optimizer, loss="mean_squared_error",
+              metrics=['accuracy'])
 
 
 # set early stopping monitor so the model stops training when it won"t improve anymore
-early_stopping_monitor = EarlyStopping(patience=30)
+early_stopping_monitor = EarlyStopping(patience=200)
 # train model
-model.fit(X_train, Y_train, validation_split=0.2,
-          epochs=10000, callbacks=[early_stopping_monitor])
+history = model.fit(X_train, Y_train, validation_split=0.2,
+                    epochs=10000, callbacks=[early_stopping_monitor])
 
 # example on how to use our newly trained model on how to make predictions on unseen data (we will pretend our new data is saved in a dataframe called "X_test").
 # Y_test_predictions = model.predict(X_test)
 # print(Y_test_predictions)
+
+# Plot training & validation accuracy values
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
