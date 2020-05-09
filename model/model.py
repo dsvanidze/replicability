7bf30603b3a1d83d1781f70413b5bbf8f6cf5d44
dsvanidze <svanskivar@gmail.com>
2020-05-09 18:26:15 +0200

    model input and variable scaling changed

--- model/model.py
+++ model/model.py
@@ -16,10 +16,6 @@ reproduce(0)
 # read in data using pandas
 data = pd.read_csv("data/csvs/cleandata-basic-model.csv")
 
-scaler = StandardScaler()
-data[data.columns] = scaler.fit_transform(data[data.columns])
-print(data.head())
-
 # create a dataframe with all training data except the target column
 X = data.drop(columns=["cases"])
 
@@ -28,6 +24,14 @@ Y = data[["cases"]]
 
 # check data has been read in properly
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+# data[data.columns, -"cases"] = scaler.fit_transform(data[data.columns, -"cases"])
+print(X_train)
+print(X_test)
 
 # create model
 model = Sequential()
@@ -78,33 +82,36 @@ model.add(Dense(1,
 model.add(BatchNormalization())
 model.add(Activation("linear"))
 
-optimizer = optimizers.Adam(learning_rate=0.0005)
+optimizer = optimizers.Adam(learning_rate=0.0002)
 
 # compile model using mse as a measure of model performance
 model.compile(optimizer=optimizer, loss="mean_squared_error")
 
 
 # set early stopping monitor so the model stops training when it won"t improve anymore
-early_stopping_monitor = EarlyStopping(patience=5000)
+# early_stopping_monitor = EarlyStopping(patience=5000)
 # train model
 history = model.fit(X_train, Y_train, batch_size=512, validation_split=0.2,
-                    epochs=100000, verbose=2, callbacks=[early_stopping_monitor])
+                    epochs=2000000, verbose=0, callbacks=[])
 
 # example on how to use our newly trained model on how to make predictions on unseen data (we will pretend our new data is saved in a dataframe called "X_test").
-Y_test_predictions = model.predict(X_test)
-# print(Y_test_predictions)
+#Y_test_predictions = model.predict(X_test)
+#print(np.squeeze(Y_test_predictions))
 
-newdata = pd.DataFrame(data={"longitude": X_test["longitude"],
-                             "latitude": X_test["latitude"],
-                             "cases": np.squeeze(Y_test_predictions),
-                             "access": X_test["access"],
-                             "longxlat": X_test["longxlat"]})
+# newdata = pd.DataFrame(data={"longitude": X_test["longitude"],
+#                              "latitude": X_test["latitude"],
+#                              "cases": np.squeeze(Y_test_predictions),
+#                              "access": X_test["access"],
+#                              "longxlat": X_test["longxlat"]})
 
-print("INVERSED: ", np.squeeze(scaler.inverse_transform(
-    [0, 0, Y_test_predictions, 0, 0])[2]))
+# print("INVERSED: ", np.squeeze(scaler.inverse_transform(
+#     [0, 0, Y_test_predictions, 0, 0])[2]))
+results_train = model.evaluate(X_train, Y_train, batch_size=512)
+print(results_train)
 
-results = model.evaluate(X_test, Y_test, batch_size=512)
-print(results)
+
+results_test = model.evaluate(X_test, Y_test, batch_size=512)
+print(results_test)
 
 model.save("models-collection/basic-model.h5")
 print("Saved model to disk")
