e2a97057761e78fe67bc37fea9b5a983efd7e17c
dsvanidze <svanskivar@gmail.com>
2020-06-06 20:50:45 +0200

    Model with all 6 covariates. Y variable logarithmized and improved model training. Batchsize set to number of Training samples. The best performance until now Model #1.

--- model/model.py
+++ model/model.py
@@ -16,6 +16,7 @@ reproduce(0)
 
 
 def r2_score(y_true, y_pred):
+    from keras import backend as K
     SS_res = K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
     return (1 - SS_res/(SS_tot + K.epsilon()))
@@ -24,7 +25,7 @@ def r2_score(y_true, y_pred):
 def r2_score_adj(y_true, y_pred):
     # TODO: check if there is better implementation of Adj. R2
     n = Lambda(lambda x: x[0]/x[1])([K.sum(y_true), K.mean(y_true)])
-    p = K.constant(n_cols)
+    p = K.constant(n_units_of_N_layer)
     one = K.constant(1)
     SS_res = K.sum(K.square(y_true - y_pred)) / (n - p - one)
     SS_tot = K.sum(K.square(y_true - K.mean(y_true))) / (n - one)
@@ -38,6 +39,11 @@ print(len(raw_data))
 data = raw_data.drop(raw_data[raw_data.sampling == 0].index).dropna().drop(
     columns=['id', 'pop', 'logpop', 'nbHF', 'adjpop', 'sampling', 'total_cases', 'elevation', 'aridity', 'irrigation'])
 print(data.head())
+
+data['adj_cases'] = np.log(data[["adj_cases"]] + 1)
+
+print(data['adj_cases'])
+
 
 
 # create a dataframe with all training data except the target column
@@ -61,10 +67,14 @@ print(X_test)
 model = Sequential()
 
 # get number of columns in training data
+validation_split_rate = 0.2
+n_units_of_N_layer = 10
 n_cols = X_train.shape[1]
+train_total = X_train.shape[0]
+batch_size = int(np.floor((1 - validation_split_rate) * train_total))
 
 # add model layers
-model.add(Dense(15,
+model.add(Dense(n_units_of_N_layer,
                 kernel_initializer='he_normal',
                 bias_initializer='zeros',
                 input_shape=(n_cols,)))
@@ -78,7 +88,7 @@ model.add(Dense(1,
 model.add(BatchNormalization())
 model.add(Activation("linear"))
 
-optimizer = optimizers.Adam(learning_rate=0.0001)
+optimizer = optimizers.Adam(learning_rate=0.00005)
 
 # compile model using mse as a measure of model performance
 model.compile(optimizer=optimizer, loss="mean_squared_error",
@@ -86,10 +96,10 @@ model.compile(optimizer=optimizer, loss="mean_squared_error",
 
 
 # set early stopping monitor so the model stops training when it won"t improve anymore
-# early_stopping_monitor = EarlyStopping(patience=5000)
+early_stopping_monitor = EarlyStopping(patience=1000)
 # train model
-history = model.fit(X_train, Y_train, batch_size=512, validation_split=0.2,
-                    epochs=30000, verbose=2, callbacks=[])
+history = model.fit(X_train, Y_train, batch_size=batch_size, validation_split=validation_split_rate,
+                    epochs=30000, verbose=1, callbacks=[early_stopping_monitor])
 
 # example on how to use our newly trained model on how to make predictions on unseen data (we will pretend our new data is saved in a dataframe called "X_test").
 #Y_test_predictions = model.predict(X_test)
@@ -103,11 +113,11 @@ history = model.fit(X_train, Y_train, batch_size=512, validation_split=0.2,
 
 # print("INVERSED: ", np.squeeze(scaler.inverse_transform(
 #     [0, 0, Y_test_predictions, 0, 0])[2]))
-results_train = model.evaluate(X_train, Y_train, batch_size=512)
+results_train = model.evaluate(X_train, Y_train, batch_size=batch_size)
 print(results_train)
 
 
-results_test = model.evaluate(X_test, Y_test, batch_size=512)
+results_test = model.evaluate(X_test, Y_test, batch_size=batch_size)
 print(results_test)
 
 # model.save("models-collection/mlp-model-basic.h5")
