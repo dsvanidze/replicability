0e2f75fa07e9a3ca40b3b091cd1165c5376543ca
dsvanidze <svanskivar@gmail.com>
2020-07-13 23:25:35 +0200

    Model Search prepared to run on the server

--- model/model-randomsearch.py
+++ model/model-randomsearch.py
@@ -7,69 +7,24 @@ from kerastuner.tuners import RandomSearch
 from tensorflow.keras import metrics
 
 import matplotlib.pyplot as plt
-from keras import backend as K
-
-from keras.callbacks import EarlyStopping, TensorBoard
-# from keras.layers import Dense, BatchNormalization, Dropout, Activation, Lambda, InputLayer
-# from keras.models import Sequential
-# from keras import optimizers
+from keras.callbacks import TensorBoard
 from keras import metrics
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from reproduce import reproduce
 import numpy as np
-import time
 from tensorboard.plugins.hparams import api as hp
 import tensorflow as tf
+from load_data import get_data
+from datetime import datetime
 
+begin = datetime.now()
 # This set seeds to make the result reproducible
 # reproduce(0)
 
-# NAME = "covid-19-spatial-prediction-MLP-{}".format(int(time.time()))
 
-tensorboard = TensorBoard(log_dir="logs/")
-
-
-# def r2_score(y_true, y_pred):
-#     from keras import backend as K
-#     SS_res = K.sum(K.square(y_true - y_pred))
-#     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
-#     return (1 - SS_res/(SS_tot + K.epsilon()))
-
-
-# def r2_score_adj(y_true, y_pred):
-#     # TODO: check if there is better implementation of Adj. R2
-#     n = Lambda(lambda x: x[0]/x[1])([K.sum(y_true), K.mean(y_true)])
-#     p = K.constant(n_units_of_N_layer)
-#     one = K.constant(1)
-#     SS_res = K.sum(K.square(y_true - y_pred)) / (n - p - one)
-#     SS_tot = K.sum(K.square(y_true - K.mean(y_true))) / (n - one)
-#     return (1 - SS_res/(SS_tot + K.epsilon()))
+# tensorboard = TensorBoard(log_dir="logs/")
 
 # read in data using pandas
-
-def get_data():
-    data = {
-        "train": pd.read_csv("data/csvs/split/train.csv", index_col="index"),
-        "validation": pd.read_csv("data/csvs/split/validation.csv", index_col="index"),
-        "test": pd.read_csv("data/csvs/split/test.csv", index_col="index"),
-    }
-
-    for key in data:
-        data[key]["longitude"] = data[key]["longitude"] / 180
-        data[key]["latitude"] = data[key]["latitude"] / 90
-        data[key]["adj_cases"] = np.log(data[key]["adj_cases"] + 1)
-
-    X_train = data["train"].drop("adj_cases", axis=1)
-    Y_train = data["train"]["adj_cases"]
-    X_validation = data["validation"].drop("adj_cases", axis=1)
-    Y_validation = data["validation"]["adj_cases"]
-    X_test = data["test"].drop("adj_cases", axis=1)
-    Y_test = data["test"]["adj_cases"]
-
-    return X_train, Y_train, X_validation, Y_validation, X_test, Y_test
-
 
 X_train, Y_train, X_validation, Y_validation, X_test, Y_test = get_data()
 
@@ -80,25 +35,29 @@ n_cols = X_train.shape[1]
 train_total = X_train.shape[0]
 batch_size = int(np.floor((1 - validation_split_rate) * train_total))
 
+print(X_train.values)
+print(Y_train.values)
+
 
 def build_model(hp):
     model = Sequential()
     model.add(InputLayer(input_shape=(n_cols,)))
 
-    for i in range(hp.Int('num_layers', 2, 10)):
-        model.add(Dense(units=hp.Int('units_' + str(i), min_value=16,
-                                     max_value=512, step=16),
+    for i in range(hp.Int('num_layers', 2, 20)):
+        model.add(Dense(units=hp.Int('units_' + str(i), min_value=32,
+                                     max_value=1024, step=32),
                         activation='relu',
-                        kernel_initializer='he_normal',
+                        kernel_initializer='glorot_uniform',
                         bias_initializer='zeros'))
         model.add(Dropout(rate=hp.Float('dropout_' + str(i), min_value=0.0,
                                         max_value=0.9, step=0.3)))
 
     model.add(Dense(1, activation='linear',
-                    kernel_initializer='he_normal',
+                    kernel_initializer='glorot_uniform',
                     bias_initializer='zeros'))
-    model.compile(optimizer=Adam(hp.Choice('learning_rate', values=[
-                  1e-2, 1e-3, 1e-4, 1e-5])), loss='mean_squared_error', metrics=[metrics.MeanSquaredError(), metrics.RootMeanSquaredError(), metrics.MeanAbsoluteError(), metrics.MeanAbsolutePercentageError(), metrics.MeanSquaredLogarithmicError()])
+    model.compile(optimizer=Adam(learning_rate=0.001),
+                  loss='mean_squared_error',
+                  metrics=[metrics.MeanSquaredError(), metrics.RootMeanSquaredError(), metrics.MeanAbsoluteError(), metrics.MeanAbsolutePercentageError(), metrics.MeanSquaredLogarithmicError()])
     return model
 
 
@@ -106,25 +65,28 @@ tuner = RandomSearch(
     build_model,
     seed=0,
     objective='val_mean_squared_error',
-    max_trials=50,
+    max_trials=500,
     executions_per_trial=1,
     directory='random-search',
     project_name='covid-19-nn')
 
 tuner.search_space_summary()
 
-# tuner.search(X_train, Y_train,
-#              epochs=1000,
-#              batch_size=train_total,
-#              validation_data=(X_validation, Y_validation),
-#              callbacks=[tensorboard])
+# Use .values to convert pandas dataframe to numpy array
+# To avoid the Warning -> WARNING:tensorflow:Falling back from v2 loop because of error: Failed to find data adapter that can handle input: <class 'pandas.core.frame.DataFrame'>, <class 'NoneType'>
+tuner.search(X_train.values, Y_train.values,
+             epochs=500,
+             batch_size=train_total,
+             validation_data=(X_validation.values, Y_validation.values),
+             callbacks=[])
 
 print("######## GET BEST MODELS ########")
 models = tuner.get_best_models()
+evaluation = models[0].evaluate(X_test.values, Y_test.values)
 print(models[0].summary())
-print(models[0].evaluate(X_test, Y_test))
+print(evaluation)
+print("Mean Squared Error", evaluation[1])
 # print(tuner.results_summary(1))
-
 
 
 # Save the best model
@@ -144,8 +106,9 @@ def custom_adj_r2(mse, Y, p):
     return 1 - (1 - r2) * standard_term
 
 
-print(custom_r2(0.5159257054328918, Y_test.to_numpy()))
-print(custom_adj_r2(0.5159257054328918, Y_test.to_numpy(), 16))
+print("R2:", custom_r2(evaluation[1], Y_test.to_numpy()))
+print("Adj. R2:", custom_adj_r2(evaluation[1], Y_test.to_numpy(), 16))
 
 print("######## SUMMARY ########")
 # tuner.results_summary(3)
+print("Overall Runtime:", datetime.now() - begin)
