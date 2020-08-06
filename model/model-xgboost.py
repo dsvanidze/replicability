c63966c8a9d046b0f6a92102dc3406431b9f998e
dsvanidze <svanskivar@gmail.com>
2020-08-06 12:29:43 +0200

    XGBoost model with the parameters recommend in best practices. Test-Set MSE: 0.5158, RMSE: 0.7182

--- model/model-xgboost.py
+++ model/model-xgboost.py
@@ -6,11 +6,11 @@ from utils import get_data
 
 X_train, Y_train, X_validation, Y_validation, X_test, Y_test = get_data()
 
-xg_reg = xgb.XGBRegressor(objective='reg:squarederror', colsample_bytree=0.5, learning_rate=0.1,
-                          max_depth=10, alpha=0.9, n_estimators=40)
-
+xg_reg = xgb.XGBRegressor(objective='reg:squarederror', learning_rate=0.1, n_estimators=100, max_depth=3, subsample=1.0)
 
 xg_reg.fit(X_train, Y_train, eval_metric="rmse", eval_set=[(X_validation, Y_validation)], verbose=True)
+
+print(xg_reg.evals_result())
 
 preds = xg_reg.predict(X_test)
 
